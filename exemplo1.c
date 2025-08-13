0ffa000d4de634b2f63adb564b9a265ff646e0c4
Yummi101o <laurashiguemori@gmail.com>
2025-08-13 01:18:21 +0000

    ano de fundação da fci

--- exemplo1.c
+++ exemplo1.c
@@ -6,6 +6,7 @@ int main()  {
 		printf("Ciência da computação\n");
 		printf("Algoritimos e Progamação II\n");
 		printf("Turma 02P\n");
+		printf("Ano de fundação da FCI foi 1970\n");
 		
 		system("pause");
 		return 0;
