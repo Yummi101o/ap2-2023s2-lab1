4871006d73ed5350fa0972aea730035bc0359ac5
Yummi101o <laurashiguemori@gmail.com>
2025-08-12 22:06:46 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()  {
+		printf("Mackenzie - FCI\n");
+		printf("Algoritimos e ");
+		printf("Prog. II\n");
+		printf("Turma 02P");
+		
+		system("pause");
+		return 0;
+		
+}
