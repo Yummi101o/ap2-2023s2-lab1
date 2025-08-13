c40fac4ecef4d6450d7f4ee9b011c4cf26f229eb
Yummi101o <laurashiguemori@gmail.com>
2025-08-12 22:38:51 -0300

    Create exercicio1.1.c

---
+++ exercicio1.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	float minimo, bruto;
+	
+	printf("Digite o salário minimo: ");
+	scanf("%d", &minimo);
+	
+	printf("Digite o seu salário: ");
+	scanf("%d" &bruto);
+	
+}
