922ea440ebe1751e1b363f9ed6dee639c9317029
Yummi101o <laurashiguemori@gmail.com>
2025-08-13 19:36:08 -0300

    Update #include <stdio.c

--- #include <stdio.c
+++ #include <stdio.c
@@ -2,11 +2,14 @@
 
 int main() {
 	float minimo, bruto;
+	soma = bruto/minimo
 	
 	printf("Digite o salário minimo: ");
 	scanf("%d", &minimo);
 	
 	printf("Digite o seu salário: ");
 	scanf("%d" &bruto);
+
+	std::cout <<"Você ganha " << soma << "salários minimos" << std::end1;
 	
 }
